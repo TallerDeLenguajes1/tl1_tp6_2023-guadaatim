aa075ed22619d7375668921aad88b4a646a1a067
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-05-27 17:49:36 -0300

    leer numeros y opciones

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -2,8 +2,32 @@
 
 //Calculadora V1
 
+Console.WriteLine("Calculadora");
+Console.WriteLine("1 - Suma");
+Console.WriteLine("1 - Resta");
+Console.WriteLine("1 - Multiplicacion");
+Console.WriteLine("1 - Division");
+Console.WriteLine("Ingrese la operacion que desea realizar: ");
 
+int opcion; 
 
+int.TryParse(Console.ReadLine(), out opcion);
 
+Console.Write(opcion);
 
+int num1;
+int num2;
 
+Console.WriteLine("ingrese un numero: ");
+int.TryParse(Console.ReadLine(), out num1);
+
+Console.WriteLine("Ingrese otro numero: ");
+int.TryParse(Console.ReadLine(), out num2);
+/*
+switch (opcion)
+{
+    case 1:
+
+    default:
+}
+*/
