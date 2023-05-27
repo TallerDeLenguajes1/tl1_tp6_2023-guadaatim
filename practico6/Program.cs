ad192f1aacfdcbf51c73a9abb74960fa6e14a346
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-05-27 20:05:33 -0300

    agregar while

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -2,55 +2,64 @@
 
 //Calculadora V1
 
-Console.WriteLine("Calculadora");
-Console.WriteLine("1 - Suma");
-Console.WriteLine("2 - Resta");
-Console.WriteLine("3 - Multiplicacion");
-Console.WriteLine("4 - Division");
-Console.WriteLine("Ingrese la operacion que desea realizar: ");
+int aux = 1;
 
-int opcion; 
-
-int.TryParse(Console.ReadLine(), out opcion);
-
-//Console.Write(opcion);
-
-int num1;
-int num2;
-int resultado = 0;
-
-Console.WriteLine("ingrese un numero: ");
-int.TryParse(Console.ReadLine(), out num1);
-
-Console.WriteLine("Ingrese otro numero: ");
-int.TryParse(Console.ReadLine(), out num2);
-
-switch (opcion)
+while (aux != 0)
 {
-    case 1:
-        resultado = num1 + num2;
-    break;
-    case 2:
-        resultado = num1 - num2;
-    break;
-    case 3:
-        //resultado = num1 * num2;
-        for (int i = 0; i < num1; i++)
-        {
-            resultado = resultado + num2;
-        }
-        resultado = num1 * num2;
-    break;
-    case 4:
-        if (num2 != 0)
-        {
-            resultado = num1 / num2;
-        } else
-        {
-            Console.WriteLine("Error");
-        }
-    break;
+    Console.WriteLine("Calculadora");
+    Console.WriteLine("1 - Suma");
+    Console.WriteLine("2 - Resta");
+    Console.WriteLine("3 - Multiplicacion");
+    Console.WriteLine("4 - Division");
+    Console.WriteLine("Ingrese la operacion que desea realizar: ");
+
+    int opcion; 
+
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    //Console.Write(opcion);
+
+    int num1;
+    int num2;
+    int resultado = 0;
+
+    Console.WriteLine("ingrese el primer numero: ");
+    int.TryParse(Console.ReadLine(), out num1);
+
+    Console.WriteLine("Ingrese el segundo numero: ");
+    int.TryParse(Console.ReadLine(), out num2);
+
+    switch (opcion)
+    {
+        case 1:
+            resultado = num1 + num2;
+        break;
+        case 2:
+            resultado = num1 - num2;
+        break;
+        case 3:
+            //resultado = num1 * num2;
+            for (int i = 0; i < num1; i++)
+            {
+                resultado = resultado + num2;
+            }
+            resultado = num1 * num2;
+        break;
+        case 4:
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+            } else
+            {
+                Console.WriteLine("Error");
+            }
+        break;
+    }
+
+    Console.WriteLine("El resultado es: ");
+    Console.Write(resultado);
+
+    Console.WriteLine("Ingrese 0 para temrinar o 1 para realizar otro calculo: ");
+    int.TryParse(Console.ReadLine(), out aux);
 }
 
-Console.WriteLine("El resultado es: ");
-Console.Write(resultado);
