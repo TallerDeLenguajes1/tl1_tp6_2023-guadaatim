3867d45476a682c477721db9929adbcc6f297154
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-05-30 22:35:37 -0300

    concatenar y extraer cadenas

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,13 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//primera cadena
 string cadena = String.Empty;
-string cadena2 = String.Empty;
 
 Console.WriteLine("Ingrese una palabra: ");
 cadena = Console.ReadLine(); //?
 
+//longitud primera cadena
 int longitudcad1 = cadena.Length;
 
 Console.WriteLine("Longitud de la cadena ingresada: " + longitudcad1);
 
+//segunda cadena
+string cadena2 = String.Empty;
+
 Console.WriteLine("Ingrese otra palabra: ");
+cadena2 = Console.ReadLine();
+
+//concatenar cadenas
+string cadenasconcatenadas = String.Empty;
+
+cadenasconcatenadas = cadena + cadena2;
+Console.WriteLine("Cadenas concatenadas: " + cadenasconcatenadas);
+
+//extraer subcadena
+
+Console.WriteLine(cadena.Substring(2, 1));
