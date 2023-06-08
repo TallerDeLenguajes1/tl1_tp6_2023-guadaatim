780e8a958cbab11d3b141ebc47dc04c7d5c18653
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-06-07 23:39:48 -0300

    controles

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -127,90 +127,96 @@ while (aux != 0)
 
     int opcion; 
 
-    int.TryParse(Console.ReadLine(), out opcion);
+    bool control = int.TryParse(Console.ReadLine(), out opcion);
 
-    Console.WriteLine("ingrese el primer numero: ");
-    float.TryParse(Console.ReadLine(), out num1);
-
-    Console.WriteLine("Ingrese el segundo numero: ");
-    float.TryParse(Console.ReadLine(), out num2);
-
-    Console.WriteLine(num1);
-
-    switch (opcion)
+    //Console.Write(opcion);
+    if (control)
     {
-        case 1:
-            resultado = num1 + num2;
-        break;
-        case 2:
-            resultado = num1 - num2;
-        break;
-        case 3:
-            resultado = num1 * num2;
-        break;
-        case 4:
-            if (num2 != 0)
+        Console.WriteLine("ingrese el primer numero: ");
+        bool control2 = float.TryParse(Console.ReadLine(), out num1);
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        bool control3 = float.TryParse(Console.ReadLine(), out num2);
+
+        if (control2 && control3)
+        {
+            switch (opcion)
             {
-                resultado = num1 / num2;
+                case 1:
+                    resultado = num1 + num2;
+                break;
+                case 2:
+                    resultado = num1 - num2;
+                break;
+                case 3:
+                    resultado = num1 * num2;
+                break;
+                case 4:
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                    } else
+                    {
+                        Console.WriteLine("Error");
+                    }
+                break;
+                default:
+                    int a;
+                    double numradian;
+                    
+                    Console.WriteLine("Elija uno de los numeros ingresados: ");
+                    Console.WriteLine("1 - " +num1);
+                    Console.WriteLine("2 - " + num2);
+                    int.TryParse(Console.ReadLine(), out a);
+                    if (a == 1)
+                    {
+                        Console.WriteLine("Valor Absoluto: " + Math.Abs(num1));
+                        Console.WriteLine("Cuadrado: " + Math.Pow(num1, 2));
+                        if (num1 >= 0)
+                        {
+                            Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(num1));
+
+                        }
+                        numradian = num1 * Math.PI / 180;
+                        Console.WriteLine("Seno: " + Math.Asin(numradian));
+                        Console.WriteLine("Coseno: " + Math.Acos(numradian));
+                        Console.WriteLine("Parte entera de float: " + Math.Truncate(num1));
+                    } else
+                    {
+                        Console.WriteLine("Valor Absoluto: " + Math.Abs(num2));
+                        Console.WriteLine("Cuadrado: " + Math.Pow(num2, 2));
+                        if (num2 >= 0)
+                        {
+                            Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(num2));
+                        }
+                        numradian = num2 * Math.PI / 180;
+                        Console.WriteLine("Seno: " + Math.Asin(numradian));
+                        Console.WriteLine("Coseno: " + Math.Acos(numradian));
+                        Console.WriteLine("Parte entera de float: " + Math.Truncate(num2));
+                    }
+                    break;
+            }
+
+            if (opcion != 5)
+            {
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("El maximo numero es: " + num1);
+                Console.WriteLine("El minimo numero es: " + num2);
             } else
             {
-                Console.WriteLine("Error");
+                Console.WriteLine("El maximo numero es: " + num2);
+                Console.WriteLine("El minimo numero es: " + num1);
             }
-        break;
-        default:
-            int a;
-            double numradian;
-            
-            Console.WriteLine("Elija uno de los numeros ingresados: ");
-            Console.WriteLine("1 - " +num1);
-            Console.WriteLine("2 - " + num2);
-            int.TryParse(Console.ReadLine(), out a);
-            if (a == 1)
-            {
-                Console.WriteLine("Valor Absoluto: " + Math.Abs(num1));
-                Console.WriteLine("Cuadrado: " + Math.Pow(num1, 2));
-                if (num1 >= 0)
-                {
-                    Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(num1));
 
-                }
-                numradian = num1 * Math.PI / 180;
-                Console.WriteLine("Seno: " + Math.Asin(numradian));
-                Console.WriteLine("Coseno: " + Math.Acos(numradian));
-                Console.WriteLine("Parte entera de float: " + Math.Truncate(num1));
-            } else
-            {
-                Console.WriteLine("Valor Absoluto: " + Math.Abs(num2));
-                Console.WriteLine("Cuadrado: " + Math.Pow(num2, 2));
-                if (num2 >= 0)
-                {
-                    Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(num2));
-                }
-                numradian = num2 * Math.PI / 180;
-                Console.WriteLine("Seno: " + Math.Asin(numradian));
-                Console.WriteLine("Coseno: " + Math.Acos(numradian));
-                Console.WriteLine("Parte entera de float: " + Math.Truncate(num2));
-            }
-            break;
+            Console.WriteLine("Ingrese 0 para terminar o 1 para realizar otro calculo: ");
+            int.TryParse(Console.ReadLine(), out aux);
+        }
     }
 
-    if (opcion != 5)
-    {
-        Console.WriteLine("El resultado es: " + resultado);
-    }
-
-    if (num1 > num2)
-    {
-        Console.WriteLine("El maximo numero es: " + num1);
-        Console.WriteLine("El minimo numero es: " + num2);
-    } else
-    {
-        Console.WriteLine("El maximo numero es: " + num2);
-        Console.WriteLine("El minimo numero es: " + num1);
-    }
-
-    Console.WriteLine("Ingrese 0 para temrinar o 1 para realizar otro calculo: ");
-    int.TryParse(Console.ReadLine(), out aux);
 }
 
 string num1s = Convert.ToString(num1);
